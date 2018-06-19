02ee27b28aad9f6002b0d02d7df9a1385e60b7fb
Eduard van Valkenburg <edvan@microsoft.com>
2018-06-19 15:34:05 +0200

    updates to Brunt

--- custom_components/cover/brunt-old.py
+++ custom_components/cover/brunt-old.py
@@ -10,6 +10,7 @@ import threading
 import requests
 import voluptuous as vol
 import json
+from brunt.brunt import BruntAPI
 
 from homeassistant.helpers import discovery
 from homeassistant.exceptions import HomeAssistantError
@@ -23,7 +24,7 @@ from homeassistant.components.cover import (
 from homeassistant.helpers.entity import Entity
 import homeassistant.helpers.config_validation as cv
 
-REQUIREMENTS = ['brunt==0.1.1']
+REQUIREMENTS = ['brunt==0.0.3']
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -33,8 +34,14 @@ OPEN_CLOSE_FEATURES = SUPPORT_OPEN | SUPPORT_CLOSE
 
 DEFAULT_CLOSED_POSITION = 0
 DEFAULT_OPEN_POSITION = 100
+DEFAULT_OPEN = 'OPEN'
+DEFAULT_CLOSE = 'CLOSE'
+DEFAULT_PARTIAL = 'PARTIALLY OPEN'
 CONF_CLOSED_POSITION = 'closed_value'
 CONF_OPEN_POSITION = 'opened_value'
+CONF_OPEN = 'open'
+CONF_CLOSE = 'close'
+CONF_PARTIAL = 'partially_open'
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({     
         vol.Required(CONF_USERNAME): cv.string,
@@ -44,11 +51,18 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
         vol.Optional(CONF_CLOSED_POSITION,
                  default=DEFAULT_CLOSED_POSITION): int,
         vol.Optional(CONF_OPEN_POSITION,
-                 default=DEFAULT_OPEN_POSITION): int
+                 default=DEFAULT_OPEN_POSITION): int,
+        vol.Optional(CONF_OPEN,
+                 default=DEFAULT_OPEN): cv.string,
+        vol.Optional(CONF_CLOSE,
+                 default=DEFAULT_CLOSE): cv.string,
+        vol.Optional(CONF_PARTIAL,
+                 default=DEFAULT_PARTIAL): cv.string
 })
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Set up the brunt component."""
+    from brunt.brunt import BruntAPI
     username = config.get(CONF_USERNAME)
     password = config.get(CONF_PASSWORD)
     device = config.get(CONF_DEVICE)
@@ -56,8 +70,11 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
 
     closed_position = config.get(CONF_CLOSED_POSITION)
     open_position = config.get(CONF_OPEN_POSITION)
+    opened = config.get(CONF_OPEN)
+    close = config.get(CONF_CLOSE)
+    partial = config.get(CONF_PARTIAL)
 
-    add_devices([BruntDevice(hass, username, password, name, device, closed_position, open_position)])
+    add_devices([BruntDevice(hass, username, password, name, device, closed_position, open_position, opened, close, partial)])
 
 
 class BruntDevice(CoverDevice):
@@ -66,9 +83,9 @@ class BruntDevice(CoverDevice):
     Contains the common logic for all Brunt devices.
     """
 
-    def __init__(self, hass, username, password, name, device, closed_position, open_position):
+    def __init__(self, hass, username, password, name, device, closed_position, open_position, opened, close, partial):
         """Init the Brunt device."""
-        from brunt import BruntAPI
+        from brunt.brunt import BruntAPI
         self._username = username
         self._password = password
         self._name = name
@@ -76,13 +93,17 @@ class BruntDevice(CoverDevice):
 
         self._closed_position = closed_position
         self._open_position = open_position
+        self._open = opened
+        self._close = close
+        self._partial = partial
 
         self._bapi = BruntAPI()
         self._bapi.login(self._username, self._password)
-        res = self._bapi.getThings()['things']
+        res = self._bapi.getThings()
         if len(res) == 0:
             raise HomeAssistantError
         self._position = None
+        self._state = None
         self._update()
 
     @property
@@ -93,35 +114,38 @@ class BruntDevice(CoverDevice):
     @property
     def is_on(self):
         """Return true if the device is on."""
-        return True
+        return self._state
 
     def _update(self):
         """Poll the current state of the device."""
-        self._position = self._bapi.getState(thing=self._device)['thing']['currentPosition']
+        state = self._bapi.getState(self._device)
+        self._position = state['currentPosition']
+        if self._position == self._closed_position:
+            self._state = self._open
+        elif self._position == self._closed_position:
+            self._state = self._close
+        else:
+            self._state = self._partial
 
     @property
     def state(self):
         """Return the state of the cover."""
-        return STATE_CLOSED if self._position == self._closed_position else STATE_OPEN
+        return STATE_CLOSED if self._state == self._close else STATE_OPEN
 
     def open_cover(self, **kwargs):
-        # try:
-        self._bapi.changePosition(self._open_position, thing=self._device)
-        # except:
-        #     return False
+        self._bapi.changePosition(self._device, self._open_position)
 
     def close_cover(self, **kwargs):
-        # try:
-        self._bapi.changePosition(self._closed_position, thing=self._device)
-        # except:
-        #     return False
+        self._bapi.changePosition(self._device, self._closed_position)
+
+    def stop_cover(self, **kwargs):
+        self.update()
+        self._bapi.changePosition(self._device, self._position)
 
     def set_cover_position(self, **kwargs):
         if ATTR_POSITION in kwargs:
-            # try:
-            self._bapi.changePosition(int(kwargs[ATTR_POSITION]), thing=self._device)
-            # except:
-                # return False
+            position = kwargs[ATTR_POSITION]
+            self._bapi.changePosition(self._device, position)
     
     @property
     def current_cover_position(self):
